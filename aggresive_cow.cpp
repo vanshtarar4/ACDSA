0ad1f7feda4ce52d5fa3a1015cf7a18574281386
vanshtarar4 <vanshtarar4@gmail.com>
2024-11-20 15:32:29 +0530

    ready

--- aggresive_cow.cpp
+++ aggresive_cow.cpp
@@ -1,3 +1,45 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
+
+bool isPossible(vector<int> &arr,  int N, int C, int minAllowedDist) {
+    int cows = 1, lastStallPos = arr[0];
+
+    for(int i=1; i<N; i++){
+        if(arr[i] - lastStallPos >= minAllowedDist ){
+            cows++;
+            lastStallPos = arr[i];
+        }
+        if(cows == C){
+            return true;
+        }
+    }
+    return false;
+}
+
+int getDistance(vector<int> &arr,  int N, int C){
+    sort(arr.begin(), arr.end());
+
+    int st = 1, end = arr[N-1]-arr[0], ans= -1;
+
+    while(st <= end){
+        int mid = st + (end - st) / 2;
+
+        if(isPossible(arr, N, C, mid)){
+            ans = mid;
+            st = mid + 1;             
+        }
+        else{
+            end = mid - 1;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int N = 5, c =3;
+    vector<int> arr = {1,2,8,4,9};
+
+    cout << getDistance(arr, N , c) << endl;
+}
--- binary_search.cpp
+++ binary_search.cpp
@@ -2,27 +2,27 @@
 #include <vector>
 using namespace std;
 
-int binary_search(vector<int> arr, int tar) { // Iterative
+int binary_search(vector<int> arr, int tar) { 
     int st = 0, end = arr.size() - 1;
 
     while (st <= end) {
         int mid = st + (end - st) / 2;
 
-        if (tar < arr[mid]) // Compare with arr[mid]
-            end = mid - 1;  // Search in the left half
-        else if (tar > arr[mid]) // Compare with arr[mid]
-            st = mid + 1;  // Search in the right half
+        if (tar < arr[mid]) 
+            end = mid - 1;  
+        else if (tar > arr[mid]) 
+            st = mid + 1;  
         else
-            return mid;     // Target found
+            return mid;   
     }
-    return -1; // Target not found
+    return -1; 
 }
 
 int main() {
     vector<int> arr1 = {-1, 3, 4, 5, 9, 11, 15};
     int target = 11;
 
-    cout << binary_search(arr1, target) << endl; // Output: 5 (index of 11)
+    cout << binary_search(arr1, target) << endl; 
     
     return 0;
 }
