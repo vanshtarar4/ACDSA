c5e936436a5f9d716f184a1fb03688364164d1b2
vanshtarar4 <vanshtarar4@gmail.com>
2024-11-20 15:14:34 +0530

    day 3

--- painters_problem.cpp
+++ painters_problem.cpp
@@ -1,9 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int minTimeTOPaint(vector<int arr, int n, int m>){
+// int minTimeTOPaint(vector<int arr, int n, int m>){
 
-}
+// }
 
 int main(){
     
